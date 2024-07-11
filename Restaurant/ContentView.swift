629e251f2225a0ba9fa9a231440328ef5013bfef
Ian <isabado@onpointglobal.com>
2024-07-10 22:06:00 -0700

    Initial Commit

---
+++ Restaurant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Restaurant
+//
+//  Created by Onpoint on 7/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Onpoint on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
